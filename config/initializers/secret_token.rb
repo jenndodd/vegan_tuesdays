17dfaafb848323cf2530f39d44d260d23f775425
Jenn Dodd <jenndodd@new-host-5.home>
2013-06-01 17:29:18 -0400

    adding back the good rails app.  wheeeeee...

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+VeganTuesdays::Application.config.secret_token = '46a0a5db419f7577b407dc30a9b326942f9839132cb02cbf1e91ab1b0721b6d8b268a8011a6a087e736acb10248d506ab254c569f7737a28c2f34b79d9e09a13'
